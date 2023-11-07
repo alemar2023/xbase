738c58fc4b444334556d47f9adc88c0cf85eb727
alemar2023 <dev@alexjweb.com>
2023-11-07 03:10:55 +0100

    users role mail dashboards

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,18 @@
+class DashboardController < ActionController::Base
+
+
+  def index
+
+  end
+  def user
+
+  end
+  def admin
+
+  end
+
+  def superadmin
+
+  end
+
+end
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,31 @@
+class PagesController < ApplicationController
+
+  def home
+  end
+
+   # ---------------- user section --------------------------------------
+  def user_orders
+    render layout: 'dashboard'
+  end
+
+  def user_whishlist
+  end
+
+
+# ---------------- admin section --------------------------------------
+  def admin
+
+  end
+  def admin_section
+    render layout: 'dashboard'
+  end
+# ---------------- SUPER ADMIN section --------------------------------------
+  def super_section
+    render layout: 'dashboard'
+  end
+
+# ---------------- VAR section --------------------------------------
+
+
+end
+
